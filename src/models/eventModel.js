86f09a3df9147b2c81bb46abdb2146f34e3fa188
Dovile-D <dovile.d.git@gmail.com>
2022-03-13 20:02:21 +0200

    add file eventController.js

---
+++ src/models/eventModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+// event has a name, date/time, description and photo
+// we can add more later
+const eventSchema = mongoose.Schema(
+  {
+    eventName: {
+      type: String,
+      required: [true, "Please write event name"],
+    },
+    dateTime: {
+      type: String,
+      default: DateTime,
+      required: [true, "Please add event date and time"],
+    },
+    eventDescription: {
+      type: String,
+      required: [true, "Please write event description"],
+    },
+    eventPhoto: {
+        data: Buffer,
+        contentType: String,
+        required: [true, "Please upload event photo"],
+      },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+module.exports = mongoose.model("Event", eventSchema);
